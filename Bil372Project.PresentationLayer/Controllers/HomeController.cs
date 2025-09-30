6ca6d433e363d552de55c68632bd015bf0a80995
Bahadir İhsan Herdem <bherdem@etu.edu.tr>
2025-09-30 13:46:00 +0300

    saved UNDO information for reset changes and discarded files

---
+++ Bil372Project.PresentationLayer/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Bil372Project.PresentationLayer.Models;
+
+namespace Bil372Project.PresentationLayer.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
