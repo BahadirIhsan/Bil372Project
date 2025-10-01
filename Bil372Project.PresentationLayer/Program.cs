37c3aeb7722b06a602551c34cfa9824dd07d661e
Bahadir İhsan Herdem <bherdem@etu.edu.tr>
2025-10-01 21:17:20 +0300

    ThirdTestingCommitt

--- Bil372Project.PresentationLayer/Program.cs
+++ Bil372Project.PresentationLayer/Program.cs
@@ -12,6 +12,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseStaticFiles();   // BU OLMALI (statik dosyaları wwwroot'tan servis eder)
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
